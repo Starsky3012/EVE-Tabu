2013660cf4d80d5aa935621858b48d81cd32a9e3
Kai Friedrich <friedrich-kai@online.de>
2020-05-29 12:03:15 +0200

    CSS anpassungen und routing

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-//res.sendFile(__dirname + "/HTML/homepage.html");
+res.sendFile(__dirname + "/assets/HTML/homepage.html");
+
 });
 
-app.use('/', express.static(path.join(__dirname,'html')));
-app.use('/assets', express.static(path.join(__dirname,'assets')));
+app.use('/', express.static(path.join(__dirname,'assets','html')));
+//app.use('/assets', express.static(path.join(__dirname,'assets')));
 
 app.get('/player/:gamesession', function(req, res){
 res.send('You just joined game session: ' + req.params.gamesession);
