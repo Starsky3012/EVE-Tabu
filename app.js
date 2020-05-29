e1efe94c55fb49a2c05c62c081a51ced4c6936ba
Kai Friedrich <friedrich-kai@online.de>
2020-05-29 11:46:54 +0200

    Ordnerstrukturaenderungen

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
+const path = require('path');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-res.sendFile(__dirname + "/HTML/homepage.html");
-
+//res.sendFile(__dirname + "/HTML/homepage.html");
 });
+
+app.use('/', express.static(path.join(__dirname,'html')));
+app.use('/assets', express.static(path.join(__dirname,'assets')));
 
 app.get('/player/:gamesession', function(req, res){
 res.send('You just joined game session: ' + req.params.gamesession);
