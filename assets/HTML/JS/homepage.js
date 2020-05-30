99cdef7a571b3579afd49724f6b1f3e7c7ac54e5
Kai Friedrich <friedrich-kai@online.de>
2020-05-30 12:05:35 +0200

    Anpassungen an der homepage.html um das Eingeben einer game-id zu ermoeglichen. Kleine Anpassung an homepage.js zum Testen

--- assets/HTML/JS/homepage.js
+++ assets/HTML/JS/homepage.js
@@ -1,3 +1,7 @@
 function createGame(){
   console.log('The create button was pressed');
+  var gamesession = new Date().getTime();
+  console.log(gamesession);
+  var name = document.getElementById('namefield').value;
+  console.log(name);
 }
