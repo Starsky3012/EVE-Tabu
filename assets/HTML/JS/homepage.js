1165439d7599e17754610587c63cdd9f401db72d
Kai Friedrich <friedrich-kai@online.de>
2020-05-30 12:32:27 +0200

    Ueberpruefungen der Namen und Game-ID Felder

--- assets/HTML/JS/homepage.js
+++ assets/HTML/JS/homepage.js
@@ -1,7 +1,35 @@
 function createGame(){
+  if(checkName()){
+    document.getElementById('errorMsg').innerHTML="You have to provide a name"
+    return;
+  }
+
   console.log('The create button was pressed');
   var gamesession = new Date().getTime();
   console.log(gamesession);
   var name = document.getElementById('namefield').value;
   console.log(name);
+}
+
+function joinGame(){
+  if(checkName()){
+    document.getElementById('errorMsg').innerHTML="You have to provide a name";
+    return;
+  }
+  if(checkID()){
+    document.getElementById('errorMsgID').innerHTML="You have to provide a game ID";
+    return;
+  }
+}
+
+function checkName(){
+  if(document.getElementById('namefield').value.trim() === ""){
+    return true;
+  }
+}
+
+function checkID(){
+  if(document.getElementById('sessionfield').value.trim() === ""){
+    return true;
+  }
 }
