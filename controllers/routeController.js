67b690bcb48d7ba45e60d9d53e00969e0c3f5da0
Kai Friedrich <friedrich-kai@online.de>
2020-06-05 15:07:58 +0200

    Anpassungen im Routing und den HTML-Tags der homepage.ejs

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -7,15 +7,26 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 var data = {"name": "Collin"};
 
 module.exports = function(app){
-
+  //routing fuer die homepage
   app.get('/', function(req, res){
   res.render('homepage.ejs')
 
   });
 
-  app.get('/:sessionID/gamemaster', function(req, res){
+  app.get('/gamemaster/:sessionID', function(req, res){
   res.render('gamemaster.ejs', {user: data});
 
+  });
+
+  app.post('/gamemaster', function(req, res){
+  res.render('gamemaster.ejs', {user: data});
+
+  });
+
+  //handler fuer das starten eines Spiels
+
+  app.post('/', urlencodedParser, function(req, res){
+    console.log(req.body);
   });
 
 }
