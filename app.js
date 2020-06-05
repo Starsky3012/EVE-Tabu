2a2cfe889540ef79e0df1c135b1a5dde0fb4d72b
Kai Friedrich <friedrich-kai@online.de>
2020-06-05 10:45:16 +0200

    Hinzufuegen einer package.json und kleinere Aenderungen

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 const path = require('path');
-var app = express();
+var bodyParser = require('body-parser');
 
+var app = express();
+//set up template engine
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
 res.sendFile(__dirname + "/assets/HTML/homepage.html");
 
 });
-
+//static files fuer datei-anfragen
 app.use('/', express.static(path.join(__dirname,'assets')));
 
 //app.use('/assets', express.static(path.join(__dirname,'assets')));
@@ -22,5 +24,6 @@ app.get('/host/:gamesession', function(req, res){
 res.send('You just joined game session: ' + req.params.gamesession);
 
 });
-
+//listening to port
 app.listen(3000);
+console.log('Now listening to port 3000');
