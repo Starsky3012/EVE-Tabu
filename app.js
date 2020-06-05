cd74d3bd4b095c1bbda86186e3851d0644480876
Kai Friedrich <friedrich-kai@online.de>
2020-06-05 10:50:51 +0200

    Auslagern der Routes in einen Controller

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 var express = require('express');
 const path = require('path');
 var bodyParser = require('body-parser');
+var routeController = require('./controllers/routeController');
 
 var app = express();
 //set up template engine
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-res.sendFile(__dirname + "/assets/HTML/homepage.html");
 
-});
 //static files fuer datei-anfragen
 app.use('/', express.static(path.join(__dirname,'assets')));
 
-//app.use('/assets', express.static(path.join(__dirname,'assets')));
+routeController(app);
 
-app.get('/player/:gamesession', function(req, res){
-res.send('You just joined game session: ' + req.params.gamesession);
-
-});
-
-app.get('/host/:gamesession', function(req, res){
-res.send('You just joined game session: ' + req.params.gamesession);
-
-});
 //listening to port
 app.listen(3000);
 console.log('Now listening to port 3000');
