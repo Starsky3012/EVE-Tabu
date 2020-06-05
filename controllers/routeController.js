8448bea9a37c78ccae776b2dd5cc8a827003928d
Kai Friedrich <friedrich-kai@online.de>
2020-06-05 13:08:06 +0200

    Auslagerung des Chats fuer player.ejs und gamemaster.ejs

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,3 +1,8 @@
+//bodyParser einbinden
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
   app.get('/', function(req, res){
